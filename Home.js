b62ef105bbfbcc467520548cf0b8bb91a140429e
saileshchitturi <93159225+saileshchitturi@users.noreply.github.com>
2022-03-16 15:45:00 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,54 @@
+import impp from './impp.jpg';
+import React from 'react'
+export default function Home(){
+
+   return (
+        <div className="Intro">
+
+
+       <center>
+           
+       <h1  style={{color:"black"}} className="intro">
+       <img src ={impp} width='500' height = '350' />
+       <br></br>
+       Hii Every one We Are from KL University, We Are pursuing B.Tech 2nd Year CSE, We have been Assigned A project related to Website's Using React,
+ Node Js, Mongo ETC. this is related to the Subject TS-SDP-1 And Here We Will be Learning All related to the Web Applications, And How to use front End 
+, Back End, All Application Related Knowledge will be Thought Here. And We Used The Same Application To Develop Here In Our Web Application.<br></br>
+
+And Our Domain In this project Is Health And Life Insurance, And We Learned All The required Entity,  And The Attributes In The Domain And It Helped as 
+making the Thre Project Easy.<br></br><br></br><br></br>
+ 
+
+
+
+Team Members<br></br><br></br>
+
+CHITTURI SAILESH <br></br>
+Thatishetty Aakash Chandra <br></br>
+Karthikeyan Varaha Raj Sai <br></br><br></br><br></br>
+
+
+Email <br></br><br></br>
+
+2000031291@kluniversity.in<br></br>
+2000031377@kluniversity.in<br></br>
+2000031662@kluniversity.in<br></br><br></br>
+
+KL Deemed to be University,<br></br>
+Greenfields, Vaddeswaram, Guntur District, -522502<br></br>
+Phone No: 8632399999<br></br>
+www.kluniversity.in<br></br>
+</h1>
+
+       <h3 style={{color:"black"}} className="intro1" >
+</h3>
+
+       <h3 style={{color:"black"}} className="intro2"> 
+ </h3>
+
+       </center>
+
+   </div>
+   );
+
+}
